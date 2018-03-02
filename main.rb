0f48f0dae39a61a7bacc7f808278320e8cba3e6a
Jose Antonio Merino <josemerinog@gmail.com>
2018-03-01 20:58:48 -0500

    segundo commit

--- main.rb
+++ main.rb
@@ -1,6 +1,51 @@
 # myapp.rb
 require 'sinatra'
 
+
+
+#routes
 get '/' do
-  'Hello world!'
+  #generar html...
+  #variables
+  @fecha = Time.now
+  "Hello world!: Hoy es #{@fecha}"
+end
+
+get '/jose' do
+  'pagina para Jose'
+end
+
+get '/:city' do # path variables
+  "esta ciudad #{params[:city]}"
+  "este pais #{params[:country]}"
+  params.inspect
+
+end
+
+# params
+
+# Query string
+# URL  ?key1 = value1 & key2 = value2
+# params[]
+
+# Variable path (es de ruby no estandard)
+
+# GET 'invitado/spacio/:name'
+
+#   param[:name]
+
+get '/dado/:numero' do
+
+  Lanzar_dado = rand(1..6)
+
+  if params[:numero].to_i == Lanzar_dado
+    erb :ganaste
+  else
+    "El FBI para tu casa."
+  end
+end
+
+
+not_found do
+  "Pa' donde vas??"
 end
